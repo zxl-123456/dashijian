044bdd0ab5b8317cc98455707fdb3ba1df38b2cd
zxl <zxl@qq.com>
2020-09-04 20:11:19 +0800

    提交index

--- assets/js/ajaxPrefilter.js
+++ assets/js/ajaxPrefilter.js
@@ -1,4 +1,17 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url)
+    //  console.log(options.url)
+    options.complete = function (res) {
+        console.log(res)
+        if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+            localStorage.removeItem('token')
+            location.href = "/login.html"
+        }
+    }
+    // 统一为有权限 接口设置header请求
+    if (options.url.indexOf('/my') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+$(function () {
+    getUserInfo();
+    function getUserInfo() {
+        $.ajax({
+            url: '/my/userinfo',
+            method: 'get',
+            /*  headers: {
+                 Authorization: localStorage.getItem('token') || ''
+             }, */
+            success: function (res) {
+                // console.log(res.data)
+                if (res.status != 0) {
+                    return "获取信息失败";
+                }
+                setAvatar(res.data)
+            }
+           
+        })
+    }
+    //渲染头像
+    function setAvatar(user) {
+        var name = user.nickname || user.username
+        $('#welcome').html('欢迎  ' + name);
+        //头像
+        if (user.user_pic != null) {
+            $('.layui-nav-img').attr('src', user.user_pic).show()
+            $('.text-avatar').hide()
+        } else {
+            var first = user.username[0].toUpperCase()
+            $('.layui-nav-img').hide()
+            $('.text-avatar').html(first)
+        }
+    }
+    //退出功能
+    $('.tuichu').on('click', function () {
+        var layer = layui.layer
+        layer.confirm('确定退出？', { icon: 3, title: '提示' }, function (index) {
+            console.log(index)
+            localStorage.removeItem('token')
+            location.href = "/login.html"
+            layer.close(index)
+        })
+    })
+})
